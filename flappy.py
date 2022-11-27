35f6154162b7d50f78c9170d6cb3dfaa4caf13d5
tanmay8146 <rv3nom@outlook.com>
2022-11-28 02:12:13 +0530

    day 3

--- flappy.py
+++ flappy.py
@@ -4,8 +4,8 @@ import time
 import random
 import os
 
-import tools
-tools.clearScreen()
+from tools import *
+clearScreen()
 
 GWIN_WIDTH = 550
 GWIN_HEIGHT = 800
@@ -157,7 +157,7 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-def draw_win(win, bird, pipes, base, score):
+def draw_win(win, birds, pipes, base, score):
     win.blit(IMG_BG, (0,0))
     for pipe in pipes:
         pipe.draw(win)
@@ -165,7 +165,107 @@ def draw_win(win, bird, pipes, base, score):
     text = STAT_FONT.render("score: "+str(score), 1, (255, 255, 255))
     win.blit(text, (GWIN_WIDTH-10-text.get_width(), 10))
     base.draw(win)
-    bird.draw(win)
+    for bird in birds:
+        bird.draw(win)
     pygame.display.update()
 
-print("NO ERROR ON MAIN MODULE!\nYOU ARE GOOD TO GO!")
+def main(genomes, config):
+    print("Flappy-AI :: running on- "+showOS())
+    print("Developer :: "+devInfo())
+    nets = []
+    ge = []
+    birds = []
+    for _, g in genomes:
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
+    base = Base(730)
+    pipes = [Pipe(730)]
+
+    win = pygame.display.set_mode((GWIN_WIDTH, GWIN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    score = 0       #Initial Score
+    run = True      #Initial State of Flappy
+
+    while run:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                quit()
+        
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind += 1
+        else:
+            run = False
+            break
+
+        for x, bird in enumerate(birds):
+            bird.move()
+            ge[x].fitness += 0.1
+
+            output = nets[x].activate((bird.y, abs(bird.y-pipes[pipe_ind].height), abs(bird.y - pipes[pipe_ind].bottom)))
+
+            if output[0] > 0.5:
+                bird.jump()
+        base.move()
+
+        rem = []
+        add_pipe = False
+        for pipe in pipes:
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+            if pipe.x + pipe.PIPE_TOP.get_width() < 0:
+                rem.append(pipe)
+            pipe.move()
+
+        if add_pipe:
+            score += 1
+            for g in ge:
+                g.fitness += 5
+            pipes.append(Pipe(600))
+            pipe.move()
+            print("current score: "+str(score))
+
+        for r in rem:
+            pipes.remove(r)
+
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() >= 730 or bird.y < 0:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
+
+        draw_win(win, birds, pipes, base, score)
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    winner = p.run(main, 50)
+
+print("NO ERROR IN MAIN MODULE!\nYOU ARE GOOD TO GO!")
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
